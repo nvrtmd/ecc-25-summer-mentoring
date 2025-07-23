1fa4ce93adb6c9367c23f09f1985c4190c399ad7
yuza <untmd.crmn@gmail.com>
2025-07-23 14:43:58 +0900

    build client with mock data1:

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userCount, setUserCount] = useState(0);
+  const [randomUser, setRandomUser] = useState([]);
+  async function fetchRandomUser() {
+    const res = await fetch("/api/matching-users");
+    const data = await res.json();
+    setRandomUser(data);
+  }
+
+  useEffect(() => {
+    fetchRandomUser();
+  }, [userCount]);
 
   return (
     <>
@@ -18,18 +28,18 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setUserCount((userCount) => userCount + 1)}>
+          Fetch Random User
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <img src={randomUser.profileImage} alt="profile" />
+        <div>{randomUser.name}</div>
+        <div>{randomUser.age}</div>
+        <div>{`"${randomUser.bio}"`}</div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { worker } from "./mocks/browser.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+if (import.meta.env.VITE_NODE_ENV === "development") {
+  worker.start().then(() => {
+    createRoot(document.getElementById("root")).render(
+      <div>
+        <App />
+      </div>
+    );
+  });
+} else {
+  // 프로덕션에서 워커 없이 실행
+  createRoot(document.getElementById("root")).render(
+    <div>
+      <App />
+    </div>
+  );
+}
