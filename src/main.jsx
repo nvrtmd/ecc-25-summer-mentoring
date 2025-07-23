13a4dbbc6dba9d81e606df300ff25c077a092e33
yuza <untmd.crmn@gmail.com>
2025-07-23 15:17:42 +0900

    refactor not to use .env file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { worker } from "./mocks/browser.js";
 
-if (import.meta.env.VITE_NODE_ENV === "development") {
+if (import.meta.env.PROD) {
+  createRoot(document.getElementById("root")).render(
+    <div>
+      <App />
+    </div>
+  );
+} else {
   worker.start().then(() => {
     createRoot(document.getElementById("root")).render(
       <div>
@@ -10,11 +16,4 @@ if (import.meta.env.VITE_NODE_ENV === "development") {
       </div>
     );
   });
-} else {
-  // 프로덕션에서 워커 없이 실행
-  createRoot(document.getElementById("root")).render(
-    <div>
-      <App />
-    </div>
-  );
 }
